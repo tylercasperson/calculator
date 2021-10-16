90bba4cc251a9b81ff866e4d93151cf8ba4f70e0
Tyler Casperson <tylercasperson@gmail.com>
2021-10-16 16:17:27 -0600

    able to handle continous calculations

--- src/components/CalculatorBody.js
+++ src/components/CalculatorBody.js
@@ -4,11 +4,44 @@ import Display from './Display';
 import Button from './Button';
 
 const CalculatorBody = () => {
-  const [total, setTotal] = useState(0);
   const [equation, setEquation] = useState([]);
+  const [total, setTotal] = useState('');
+  const [firstSet, setFirstSet] = useState([]);
+  const [secondSet, setSecondSet] = useState([]);
+  const [symbolArr, setSymbolArr] = useState([]);
+
+  const resetAll = () => {
+    setTotal(0);
+    setEquation([]);
+  };
+
+  const numberSetup = (number) => {
+    if (symbolArr.length === 0) {
+      firstSet.push(number);
+    } else {
+      secondSet.push(number);
+    }
+    equation.push(number);
+    setTotal(number);
+  };
+
+  const symbolsUsed = (symbol) => {
+    if (equation.indexOf('=') === equation.length - 1) {
+      setEquation([total, symbol]);
+      setFirstSet([total]);
+      setSecondSet([]);
+      setSymbolArr([symbol]);
+    } else {
+      symbolArr.push(symbol);
+      equation.push(symbol);
+    }
+
+    setTotal(symbol);
+  };
 
   const calculation = (input) => {
     const signs = (sign, firstSet, secondSet) => {
+      console.log(sign);
       switch (sign) {
         case '+':
           return Number(firstSet) + Number(secondSet);
@@ -23,57 +56,44 @@ const CalculatorBody = () => {
       }
     };
 
-    if (input === '=') {
-      let firstSet = [];
-      let symbolArr = [];
-      let secondSet = [];
-      for (let i = 0; i < equation.length; i++) {
-        if (!isNaN(equation[i]) || equation[i] === '.') {
-          if (symbolArr.length === 0) {
-            firstSet.push(equation[i]);
-          } else if (symbolArr.length !== 0) {
-            secondSet.push(equation[i]);
-          }
-        } else {
-          symbolArr.push(equation[i]);
-        }
-      }
-
+    if (equation.indexOf('=') === -1) {
       setTotal(
         signs(symbolArr.join(''), firstSet.join(''), secondSet.join(''))
       );
-    } else {
-      setTotal(input);
     }
+
     equation.push(input);
   };
 
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', width: '40vw' }}>
-      <Display total={total} />
+      <Display
+        total={total.length === '' ? 0 : total}
+        inputHistory={equation.join('')}
+      />
       <div style={{ width: '30rem' }}>
-        <Button value={'AC'} onClick={() => calculation()} />
+        <Button value={'AC'} onClick={() => resetAll()} />
         <Button value={'+/-'} />
         <Button value={'%'} />
-        <Button value={'/'} onClick={() => calculation('/')} />
+        <Button value={'/'} onClick={() => symbolsUsed('/')} />
 
-        <Button value={7} onClick={() => calculation(7)} />
-        <Button value={8} onClick={() => calculation(8)} />
-        <Button value={9} onClick={() => calculation(9)} />
-        <Button value={'*'} onClick={() => calculation('*')} />
+        <Button value={7} onClick={() => numberSetup(7)} />
+        <Button value={8} onClick={() => numberSetup(8)} />
+        <Button value={9} onClick={() => numberSetup(9)} />
+        <Button value={'*'} onClick={() => symbolsUsed('*')} />
 
-        <Button value={4} onClick={() => calculation(4)} />
-        <Button value={5} onClick={() => calculation(5)} />
-        <Button value={6} onClick={() => calculation(6)} />
-        <Button value={'-'} onClick={() => calculation('-')} />
+        <Button value={4} onClick={() => numberSetup(4)} />
+        <Button value={5} onClick={() => numberSetup(5)} />
+        <Button value={6} onClick={() => numberSetup(6)} />
+        <Button value={'-'} onClick={() => symbolsUsed('-')} />
 
-        <Button value={1} onClick={() => calculation(1)} />
-        <Button value={2} onClick={() => calculation(2)} />
-        <Button value={3} onClick={() => calculation(3)} />
-        <Button value={'+'} onClick={() => calculation('+')} />
+        <Button value={1} onClick={() => numberSetup(1)} />
+        <Button value={2} onClick={() => numberSetup(2)} />
+        <Button value={3} onClick={() => numberSetup(3)} />
+        <Button value={'+'} onClick={() => symbolsUsed('+')} />
 
-        <Button value={0} onClick={() => calculation(0)} width={'50%'} />
-        <Button value={'.'} onClick={() => calculation('.')} />
+        <Button value={0} onClick={() => numberSetup(0)} width={'50%'} />
+        <Button value={'.'} onClick={() => numberSetup('.')} />
         <Button value={'='} onClick={() => calculation('=')} />
       </div>
     </div>
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,16 +2,28 @@ import React from 'react';
 
 const Display = (props) => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        width: '40vw',
-        height: '3rem',
-        border: '1pt solid black',
-        justifyContent: 'flex-end',
-      }}
-    >
-      {props.total}
+    <div style={{ border: '1pt solid black' }}>
+      <div
+        style={{
+          display: 'flex',
+          padding: '0 3px 1px 0px',
+          height: '2rem',
+          justifyContent: 'flex-end',
+        }}
+      >
+        {props.inputHistory}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          padding: '0 3px 3px 0px',
+          width: '40vw',
+          height: '1rem',
+          justifyContent: 'flex-end',
+        }}
+      >
+        {props.total}
+      </div>
     </div>
   );
 };
